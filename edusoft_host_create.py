5ada7ccb7bbcdc8c4426395e863ec1e7246f0ad4
juancaseres <139663972+juancaseres@users.noreply.github.com>
2025-02-13 20:22:37 -0400

    Nuevos cambios. MAC address incorporado

--- edusoft_host_create.py
+++ edusoft_host_create.py
@@ -17,52 +17,7 @@ url = "http://10.177.255.28/zabbix/api_jsonrpc.php"
 username = "Monitoreo"
 password = "MonitoreS1mpl3##"
 
-# Diccionario de IDs
-mapa_ids = {
-    "Los teques": "32",
-    "Maracay": "33",
-    "Valencia": "36",
-    "Barquisimeto": "37",
-    "Caracas": "39",
-    
-    "Feeder-LTQ01": "40",
-    "Feeder-LTQ03": "41",
-    "Feeder-SAN03": "45",
-    "Feeder-MAY01": "69",
-    "Feeder-MAY06": "70",
-    "Feeder-BTO14": "71",
-    "Feeder-CQT01": "72",
-    "Feeder-BTO12": "73",
-    "Feeder-BTO07": "74",
-    "Feeder-MAY05": "75",
-    "Feeder-MAY02": "77",
-    "Feeder-MAY04": "78",
-    "Feeder-MAY03": "79",
-    "Feeder-SD01": "81",
-    "Feeder-SD03": "82",
-    "Feeder-SD02": "93",
-
-    
-    "LTQ-OLT1": "42",
-    "LTQ-OLT2": "43",
-    "LTQ-OLT4": "54",
-    "BTO-OLT1": "46",
-    "BTO-OLT2": "47",
-    "BTO-OLT4": "49",
-    "LTQ-OLT4": "54",
-    "MCY-OLT1": "57",
-    "MCY-OLT2": "58",
-    "MCY-OLT3": "59",
-    "MCY-OLT4": "60",
-    "MCY-OLT5": "61",
-    "MCY-OLT6": "62",
-    "VAL-OLT2": "64",
-    "VAL-OLT5": "67",
-    "SAT-01": "85",
-    "SBG-01": "83",
-    "VTA-01": "95"
-}
-
+# Función para iniciar sesión en Zabbix
 def login_zabbix(url, username, password):
     data = {
         "jsonrpc": "2.0",
@@ -79,7 +34,8 @@ def login_zabbix(url, username, password):
     else:
         raise Exception(f"Error al iniciar sesión: {response_json}")
 
-def create_host(url, auth_token, hostname, hostip, groupids, contact, address, lat, lon, notes):
+# Función para crear conexión en Zabbix (Estructura JSON)
+def create_host(url, auth_token, hostname, hostip, mac_add, groupids, contact, address, lat, lon, notes):
     data = {
         "jsonrpc": "2.0",
         "method": "host.create",
@@ -93,6 +49,7 @@ def create_host(url, auth_token, hostname, hostip, groupids, contact, address, l
             ],
             "inventory_mode": 0,
             "inventory": {
+                "macaddress_a": mac_add,
                 "contact": contact,
                 "location": address,
                 "location_lat": lat,
@@ -112,23 +69,79 @@ def create_host(url, auth_token, hostname, hostip, groupids, contact, address, l
     else:
         return f"Host creado exitosamente. Host ID: {response_json['result']['hostids'][0]}"
 
+# Función para eliminar acentos y caracteres no reconocidos por Zabbix
 def quitar_acentos(cadena):
     if not isinstance(cadena, str):
         return cadena
     cadena = unicodedata.normalize("NFKD", cadena)
     return "".join(c for c in cadena if not unicodedata.combining(c)).replace("ñ", "n").replace("Ñ", "N")
 
-def obtener_ids(localidad, olt, feeder):
-    ids = ["35", "34", mapa_ids.get(localidad, "ID no encontrado"), mapa_ids.get(olt, "ID no encontrado")]
-    if localidad != "Caracas":
-        ids.append(mapa_ids.get(feeder, "ID no encontrado"))
+# Función para extraer los grupos de hosts creados en Zabbix con sus respectivos IDs
+def get_host_groups(url, token):
+    """
+    Obtiene una lista de todos los grupos de hosts y sus IDs.
+    """
+    data = {
+        "jsonrpc": "2.0",
+        "method": "hostgroup.get",
+        "params": {},
+        "auth": token,
+        "id": 2
+    }
+    headers = {"Content-Type": "application/json"}
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    response_json = response.json()
+    if "result" in response_json:
+        return response_json["result"]
+    else:
+        raise Exception(f"Error al obtener los grupos de hosts: {response_json}")
+
+# Función para mapear los grupos de la conexión cliente con los IDs registrados en Zabbix
+def obtener_ids(localidad, olt, feeder, group_ids_dict):
+    
+    group_loc = {
+        "Los teques": "Clientes FTTH POC (Los Teques)",
+        "Maracay": "Clientes FTTH POC (Maracay)",
+        "Valencia": "Clientes FTTH POC (Valencia)",
+        "Barquisimeto": "Clientes FTTH POC (Barquisimeto)",
+        "Caracas": "Clientes FTTH POC (Caracas)"
+    }
+
+    ids = ["35", "34"]
+
+    if group_loc.get(localidad) in group_ids_dict:
+        ids.append(str(group_ids_dict[group_loc.get(localidad)]))
+    else:
+        raise ValueError(f"Localidad: '{group_loc.get(localidad)}' no definido en Zabbix")
+
+    if olt in group_ids_dict:
+        ids.append(str(group_ids_dict[olt]))
+    else:
+        raise ValueError(f"OLT: '{olt}' no definida en Zabbix")
+
+    if feeder not in ["No aplica", "N/A"]:
+        if feeder in group_ids_dict:
+            ids.append(str(group_ids_dict[feeder]))
+        else:
+            raise ValueError(f"Feeder '{feeder}' no encontrado en group_ids_dict")
+
     ids.append("90")
     return ids
 
+# Función principal que permite extraer una lista con los resultados de la creación de las conexiones clientes en Zabbix y sus posibles errores
 def process_excel(file_path):
+
+    mapa_loc = {
+        "Los teques": "LTQ OSS",
+        "Maracay": "MCY OSS",
+        "Valencia": "VAL OSS",
+        "Barquisimeto": "BTO OSS",
+        "Caracas": "CCS OSS",
+    }
+
     columns = [
         "Nombre", "Customer", "Localidad", "OLT", "Feeder", 
-        "NAP", "ONT/ONU", "Dirección IP", "Ubicación de la caja NAP (Coordenadas)", 
+        "NAP", "ONT/ONU", "Dirección IP", "MAC address", "Ubicación de la caja NAP (Coordenadas)", 
         "Dirección", "Numero de telefono"
     ]
     df = pd.read_excel(file_path, usecols=columns, dtype={"Numero de telefono": str})
@@ -139,28 +152,34 @@ def process_excel(file_path):
     df["Nombre"] = df["Nombre"].str.strip()
     df["Customer"] = df["Customer"].str.strip()
     df["ONT/ONU"] = df["ONT/ONU"].str.strip()
-    df["hostname"] = df["Nombre"] + " " + df["ONT/ONU"] + " ID" + df["Customer"] 
-    data_dict = df.to_dict(orient="records")
+    df["hostname"] = df["Nombre"] + " " + df["ONT/ONU"] + " ID"     + df["Customer"] + " " + df["Localidad"].map(mapa_loc)
+    client_data_dict = df.to_dict(orient="records")
 
     try:
         auth_token = login_zabbix(url, username, password)
     except Exception as e:
         return f"Error al iniciar sesión en Zabbix: {e}"
+    
+    host_groups = get_host_groups(url, auth_token)
+    zabbix_group_ids = {group['name'].strip(): group['groupid'].strip() for group in host_groups}
 
     resultados = []
-    for row in data_dict:
+    for row in client_data_dict:
         try:
             hostname = row["hostname"]
-            groupids = obtener_ids(row["Localidad"], row["OLT"], row["Feeder"])
+            groupids = obtener_ids(row["Localidad"], row["OLT"], row["Feeder"], zabbix_group_ids)
             latitud = row["Ubicación de la caja NAP (Coordenadas)"].split(",")[0].strip()
             longitud = row["Ubicación de la caja NAP (Coordenadas)"].split(",")[1].strip()
             response = create_host(
-                url, auth_token, hostname, row["Dirección IP"], groupids, 
-                row["Numero de telefono"], row["Dirección"], latitud, longitud, row["NAP"]
+                url, auth_token, hostname, row["Dirección IP"].strip(), row["MAC address"], groupids, 
+                row["Numero de telefono"], row["Dirección"].strip(), latitud, longitud, row["NAP"]
             )
             resultados.append(response)
+        except ValueError as e:
+            resultados.append(f"Error en los datos suministrados: {row['hostname']}: {e}")
         except Exception as e:
             resultados.append(f"Error al crear host {row['hostname']}: {e}")
+
     return resultados
 
 @app.route("/", methods=["GET", "POST"])
@@ -181,4 +200,5 @@ def upload_file():
     return render_template("upload.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
+
